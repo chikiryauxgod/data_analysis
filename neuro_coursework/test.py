85b891d6cd68b385196ea31cfcef7472f650a0c2
Семенов Лев <lewace37@yandex.ru>
2024-11-19 16:32:53 +0500

    test

--- neuro_coursework/test.py
+++ neuro_coursework/test.py
@@ -1,3 +1,16 @@
 import torch 
+import numpy as np
 print(torch.__version__)
 
+data = np.array([[1, 2, 3], [4, 5, 6]])
+#print(data)
+
+t1 = torch.from_numpy(data) #torch.from_numpy()
+print(t1)
+
+t2 = torch.tensor(data) #torch.tensor()
+#print(t2)
+
+data[0, 0] = 100
+print(t1)
+print(data)
